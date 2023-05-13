28931fe2b58beade421bd740ce32ec5422a52ed7
jpwiig <jpwiig@gmail.com>
2023-05-13 15:41:02 +0200

    exam2021 template

---
+++ eksamen2021/src/main/java/com/nito/eksamen2021/Eksamen2021Application.java
@@ -0,0 +1,13 @@
+package com.nito.eksamen2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamen2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eksamen2021Application.class, args);
+	}
+
+}
---
+++ eksamen2021/src/test/java/com/nito/eksamen2021/Eksamen2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nito.eksamen2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksamen2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
